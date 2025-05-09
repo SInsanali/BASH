125e2349090246f48d3718be496a1d5c21cd3d05
Sam <tacosinmybelly@gmail.com>
2025-05-09 07:30:49 -0600

    revamped script to actually work

--- lazy_ssh/lash_ssh.sh
+++ lazy_ssh/lash_ssh.sh
@@ -7,7 +7,8 @@
 ##################################################################################################
 ##################################################################################################
 # Description:
-# This script allows the user to select an environment to SSH into from a predefined list.
+# This script lets you select a known SSH alias and connect via your ~/.ssh/config on AlmaLinux.
+# It assumes your SSH configuration is properly set up with matching aliases.
 ##################################################################################################
 ##################################################################################################
 
@@ -16,34 +17,60 @@ GREEN="\e[32m"
 RED="\e[31m"
 RESET="\e[0m"
 
-# Define SSH environments (nicknames and hostnames)
-# Format: [number]="Nickname:hostname"
-declare -A ENVS=(
-    #[1]="Dev:dev.example.com"
-    #[2]="Staging:staging.example.com"
-    #[3]="Prod:prod.example.com"
+# Display notice
+echo -e "\n${GREEN}#######################################################################${RESET}"
+echo -e "${GREEN} ENSURE THE SSH ALIASES BELOW MATCH ENTRIES IN YOUR ~/.ssh/config FILE${RESET}"
+echo -e "${GREEN}#######################################################################${RESET}\n"
+
+# INSTRUCTIONS:
+# Your ~/.ssh/config file should contain entries like the following:
+#
+# Host alias-1
+#     HostName your.hostname.com
+#     User your-username
+#     IdentityFile ~/.ssh/your-key.pem
+#
+# Host alias-2
+#     HostName another.host.com
+#     User your-username
+#     IdentityFile ~/.ssh/another-key.pem
+#
+# These aliases (alias-1, alias-2, etc.) should match exactly in the ALIASES array below.
+
+# Define environment names and SSH aliases
+NAMES=(
+  "Environment_1"
+  "Environment_2"
+  "Environment_3"
+  "Environment_4"
+)
+
+ALIASES=(
+  "alias-1"
+  "alias-2"
+  "alias-3"
+  "alias-4"
 )
 
 # Display environment options
-echo -e "\n${GREEN}[ + ]${RESET} Select the environment to SSH into:"
-printf "\n%-5s %-20s %-20s\n" "Num" "Name" "Hostname"
-echo "----------------------------------------------------------"
-for i in "${!ENVS[@]}"; do
-    IFS=":" read -r name host <<< "${ENVS[$i]}"
-    printf "%-5s %-20s %-20s\n" "$i" "$name" "$host"
+echo -e "${GREEN}[ + ]${RESET} Select the environment to SSH into:"
+printf "\n%-5s %-20s %-25s\n" "Num" "Name" "SSH Alias"
+echo "---------------------------------------------------------------"
+
+for i in "${!NAMES[@]}"; do
+    num=$((i + 1))
+    printf "%-5s %-20s %-25s\n" "$num" "${NAMES[$i]}" "${ALIASES[$i]}"
 done
 
-# Prompt for input
+# Prompt for user input
 read -p $'\n[ ? ] Enter the number of the host you want to SSH to: ' choice
 
-# Validate selection
-if [[ -z "${ENVS[$choice]}" ]]; then
+# Validate choice
+index=$((choice - 1))
+if [[ "$choice" =~ ^[0-9]+$ ]] && (( choice >= 1 && choice <= ${#NAMES[@]} )); then
+    echo -e "\n${GREEN}[ + ]${RESET} Connecting to ${ALIASES[$index]} ..."
+    ssh "${ALIASES[$index]}"
+else
     echo -e "\n${RED}[ ! ]${RESET} Invalid selection. Exiting."
     exit 1
 fi
-
-# Extract hostname and connect
-selected="${ENVS[$choice]}"
-hostname="${selected#*:}"
-echo -e "\n${GREEN}[ + ]${RESET} Connecting to $hostname ..."
-ssh "$hostname"
